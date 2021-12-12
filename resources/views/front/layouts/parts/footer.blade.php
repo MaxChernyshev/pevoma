55ab7bc638c00def6b39f52e80324aa322660cbe
MaxChernyshev <maksim.chernyshev@gmail.com>
2021-12-12 21:53:29 +0200

    first commit

---
+++ resources/views/front/layouts/parts/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="main-footer fixed-bottom">
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12 text-dark text-center">
+                <h6>
+                    <small class="text-muted">
+                        Copyright © {{ now()->year }} <a href="https://maksimchernyshev.com" target="_blank">Max</a>. All rights reserved.
+                    </small>
+                </h6>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="{{ asset('js/front/js/app.js') }}"></script>
+
+</body>
+</html>
---
+++ resources/views/front/welcome.blade.php
@@ -0,0 +1,21 @@
+@extends('front.layouts.layout')
+
+@section('content')
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 mt-5">
+                <img class="mx-auto d-block" src="{{ asset('/img/pevoma_logo.jpg') }}" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid mt-5">
+        <div class="row justify-content-center">
+            <div class="col-12 mx-auto d-block">
+                <h1 class="text-dark text-center">САЙТ НА РЕКОНСТРУКЦИИ</h1>
+            </div>
+        </div>
+    </div>
+
+@endsection
